e5ed5f7ca517ac9933481f1a424c57bc03701e91
lowzzy <60597773+lowzzy@users.noreply.github.com>
2022-12-15 04:40:02 +0900

    add gas reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,21 @@
-// require("@nomicfoundation/hardhat-toolbox");
-
-// /** @type import('hardhat/config').HardhatUserConfig */
-// module.exports = {
-//   solidity: "0.8.17",
-// };
-
 require('@nomiclabs/hardhat-waffle');
+require('hardhat-gas-reporter');
 
 module.exports = {
   solidity: '0.8.4',
   // https://hardhat.org/metamask-issue.html
   networks: {
     hardhat: {
-      chainId: 1338,
+      chainId: 137,
     },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'JPY',
+    showTimeSpent: true,
+    showMethodSig: true,
+    coinmarketcap: /* API KEY */,
+    gasPriceApi:
+      'https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice',
   },
 };
